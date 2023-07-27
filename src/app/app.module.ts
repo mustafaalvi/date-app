ff0515889c9b7ace47eebe10ebb4a210d8d91e5c
Mustafa <mustafa.alvi.98@gmail.com>
2023-07-27 00:41:00 -0400

    Material Navigation Component with route links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,26 @@ import { MessagesComponent } from './messages/messages.component';
 import { ProfilesComponent } from './profiles/profiles.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessagesComponent,
     ProfilesComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    RouterModule, BrowserAnimationsModule
+    RouterModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
