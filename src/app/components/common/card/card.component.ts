bf80cf64db21369399b2d5c9beb3ff5fe06dbb3b
Mustafa <mustafa.alvi.98@gmail.com>
2024-07-27 23:04:36 -0400

    Refactor card impl used in Discovery component

--- src/app/components/common/card/card.component.ts
+++ src/app/components/common/card/card.component.ts
@@ -1,13 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'] 
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
   //todo: consider giving these a real type 
   @Input() image: string | undefined;
   @Input() name: string | undefined;
   @Input() description: string | undefined;
+
+  profiles: any[];
+
+  constructor(private profileService: ProfileService) { 
+    this.profiles = [];
+  }
+
+  ngOnInit(): void {
+    // Fetch profiles from the service
+    this.profileService.getProfiles().subscribe(
+      (data:any[]) => {
+        this.profiles = data;
+      }
+    );
+  }
+
+  likeProfile(profile: any): void {
+    // Handle like action
+  }
+
+  passProfile(profile: any): void {
+    // Handle pass action
+  }
 }
